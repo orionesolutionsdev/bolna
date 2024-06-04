933c9a8fbc9e509b6a0d58c677d6aff2bb2df524
Satyam Mishra <shivammishraiiith@gmail.com>
2024-06-04 13:52:46 +0530

    remove auth from executions

--- local_setup/endpoints/agent_executions.py
+++ local_setup/endpoints/agent_executions.py
@@ -11,13 +11,11 @@ router = APIRouter()
 @router.get("/agent/{agent_id}/executions")
 async def get_agent(agent_id: str, header:Request):
     try:
-        # user_id = get_user_id_from_Token(header)
-        if user_id:
-            agent_data = db[settings.EXECUTION_COLLECTION].find({"agent_id": agent_id}, {'_id':0})
-            if agent_data:
-                return JSONResponse(content=list(agent_data), status_code=200)
-            else:
-                return JSONResponse(content={"message": "Agent not found"}, status_code=404)
+        agent_data = db[settings.EXECUTION_COLLECTION].find({"agent_id": agent_id}, {'_id':0})
+        if agent_data:
+            return JSONResponse(content=list(agent_data), status_code=200)
+        else:
+            return JSONResponse(content={"message": "Agent not found"}, status_code=404)
     except Exception as e:
         return JSONResponse(content={"message": str(e)}, status_code=500)
 
@@ -26,12 +24,10 @@ async def get_agent(agent_id: str, header:Request):
 @router.get("/agent/executions/{run_id}")
 async def get_agent(run_id: str, header:Request):
     try:
-        # user_id = get_user_id_from_Token(header)
-        if user_id:
-            agent_data = db[settings.EXECUTION_COLLECTION].find_one({"run_id": run_id}, {'_id':0})
-            if agent_data:
-                return JSONResponse(content=agent_data, status_code=200)
-            else:
-                return JSONResponse(content={"message": "Run ID not found"}, status_code=404)
+        agent_data = db[settings.EXECUTION_COLLECTION].find_one({"run_id": run_id}, {'_id':0})
+        if agent_data:
+            return JSONResponse(content=agent_data, status_code=200)
+        else:
+            return JSONResponse(content={"message": "Run ID not found"}, status_code=404)
     except Exception as e:
         return JSONResponse(content={"message": str(e)}, status_code=500)
