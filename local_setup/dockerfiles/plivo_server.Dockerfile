24f57ad694ec7d7727e49e3723516ddba79f058f
Het Vaghasia <hetvaghasia39@gmail.com>
2024-08-05 17:45:30 +0530

    feat: plivo integration

--- local_setup/dockerfiles/plivo_server.Dockerfile
+++ local_setup/dockerfiles/plivo_server.Dockerfile
@@ -1,11 +1,8 @@
 FROM python:3.10.13-slim
-
 WORKDIR /app
-COPY ./requirements.txt /app
-COPY ./telephony_server/plivo_api_server.py /app
-
-RUN pip install --no-cache-dir -r requirements.txt
-
+COPY telephony_server/requirements.txt /app
+RUN --mount=type=cache,target=/root/.cache/pip pip install --no-cache-dir -r requirements.txt
+COPY telephony_server/plivo_api_server.py config.py /app/
+COPY vo_utils /app/vo_utils
 EXPOSE 8002
-
 CMD ["uvicorn", "plivo_api_server:app", "--host", "0.0.0.0", "--port", "8002"]
--- local_setup/telephony_server/plivo_api_server.py
+++ local_setup/telephony_server/plivo_api_server.py
@@ -1,11 +1,15 @@
 import os
 import json
+from datetime import datetime
 import requests
 import uuid
+from vo_utils.database_utils import db
 from dotenv import load_dotenv
 import redis.asyncio as redis
+from config import settings
 from fastapi import FastAPI, HTTPException, Query, Request
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
 import plivo
 
 app = FastAPI()
@@ -15,6 +19,15 @@ port = 8002
 plivo_auth_id = os.getenv('PLIVO_AUTH_ID')
 plivo_auth_token = os.getenv('PLIVO_AUTH_TOKEN')
 plivo_phone_number = os.getenv('PLIVO_PHONE_NUMBER')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 # Initialize Plivo client
 plivo_client = plivo.RestClient(os.getenv('PLIVO_AUTH_ID'), os.getenv('PLIVO_AUTH_TOKEN'))
@@ -43,7 +56,15 @@ async def make_call(request: Request):
     try:
         call_details = await request.json()
         agent_id = call_details.get('agent_id', None)
-
+        from_number = call_details.get('from_number', plivo_phone_number)
+        recipient_data = call_details.get('recipient_data', None)
+        context_id =  str(uuid.uuid4())
+        data_for_db ={
+            'context_id': context_id,
+            'created_at': datetime.now().isoformat(),
+            'recipient_data': recipient_data
+            }
+        db[settings.CALL_CONTEXTS].insert_one(data_for_db)
         if not agent_id:
             raise HTTPException(status_code=404, detail="Agent not provided")
 
@@ -57,14 +78,23 @@ async def make_call(request: Request):
 
         # adding hangup_url since plivo opens a 2nd websocket once the call is cut.
         # https://github.com/bolna-ai/bolna/issues/148#issuecomment-2127980509
-        call = plivo_client.calls.create(
-            from_=plivo_phone_number,
-            to_=call_details.get('recipient_phone_number'),
-            answer_url=f"{telephony_host}/plivo_connect?bolna_host={bolna_host}&agent_id={agent_id}",
-            hangup_url=f"{telephony_host}/plivo_hangup_callback",
-            answer_method='POST')
+        try:
+            call = plivo_client.calls.create(
+                from_=plivo_phone_number,
+                to_=call_details.get('recipient_phone_number'),
+                answer_url=f"{telephony_host}/plivo_connect?bolna_host={bolna_host}&agent_id={agent_id}&context_id={context_id}",
+                hangup_url=f"{telephony_host}/plivo_hangup_callback",
+                answer_method='POST')
+            print(f'Call initiated: {call}', call.__dict__)
+        except Exception as e:
+            print(f'make_call exception: {str(e)}')
+        response_data = {
+            "agent_id": agent_id,
+            "sid": call.request_uuid,
+            "to_phone": call_details.get('recipient_phone_number'),
+        }
 
-        return PlainTextResponse("done", status_code=200)
+        return JSONResponse(content=response_data, status_code=200)
 
     except Exception as e:
         print(f"Exception occurred in make_call: {e}")
@@ -72,14 +102,14 @@ async def make_call(request: Request):
 
 
 @app.post('/plivo_connect')
-async def plivo_connect(request: Request, bolna_host: str = Query(...), agent_id: str = Query(...)):
+async def plivo_connect(request: Request, bolna_host: str = Query(...), agent_id: str = Query(...), context_id: str = Query(...)):
     try:
-        bolna_websocket_url = f'{bolna_host}/chat/v1/{agent_id}'
-
+        bolna_websocket_url = f'{bolna_host}/chat/v1/{agent_id}/{context_id}'
+        # <Stream>
+        # <MultiPartyCall role="customer">mpc_name</MultiPartyCall>    
         response = '''
         <Response>
-            <Stream bidirectional="true">{}</Stream>
-            <MultiPartyCall role="customer">mpc_name</MultiPartyCall>    
+            <Stream bidirectional="true" keepCallAlive="true">{}</Stream>
         </Response>
         '''.format(bolna_websocket_url)
 
