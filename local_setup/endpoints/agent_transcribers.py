065f348bb55d0eebe09cd4733eda81a508508486
Puneet Singla <puneet.singla@orionesolutions.com>
2024-08-08 15:50:34 +0530

    agent transcriber update

--- local_setup/endpoints/agent_transcribers.py
+++ local_setup/endpoints/agent_transcribers.py
@@ -9,7 +9,7 @@ class TranscriberRes(Transcriber):
 
 # Initial transcribers data
 transcribers = [
-    TranscriberRes(sampling_rate=16000, endpointing=400, keywords="", stream=True, model="nova-2-general-en", language="en", encoding="linear16", provider="deepgram", task="transcribe", id="nova-2-general"),
+    TranscriberRes(sampling_rate=16000, endpointing=400, keywords="", stream=True, model="nova-2-general", language="en", encoding="linear16", provider="deepgram", task="transcribe", id="nova-2-en-general"),
     TranscriberRes(sampling_rate=16000, endpointing=400, keywords="", stream=True, model="nova-2-general", language="hi", encoding="linear16", provider="deepgram", task="transcribe", id="nova-2-hi-general"),
     TranscriberRes(sampling_rate=16000, endpointing=400, keywords="", stream=True, model="nova-2-meeting", language="en", encoding="linear16", provider="deepgram", task="transcribe", id="nova-2-meeting"),
     TranscriberRes(sampling_rate=16000, endpointing=400, keywords="", stream=True, model="nova-2-phonecall", language="en", encoding="linear16", provider="deepgram", task="transcribe", id="nova-2-phonecall"),
